651bf591be62cdb8026d7ba5d99e048d24067c15
Sushil <grayson.9999@gmail.com>
2024-07-22 21:49:55 +1000

    Implementing onChange event for form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { UserForm } from "./components/UserForm";
+import { UserTable } from "./components/UserTable";
 
 const App = () => {
   return (
-    <div>
+    <div className="container pt-4">
+      <h2 className="text-center">UserList</h2>
+      <hr />
       {/* form component   */}
       <UserForm />
+
       {/* table component  */}
+
+      <UserTable />
     </div>
   );
 };
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,28 +1,40 @@
 import React from "react";
 
 export const UserForm = () => {
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+  };
   return (
-    <div className="w-50 shadow-lg p-5 m-auto mt-5 rounded">
+    <div className="w-50 shadow-lg p-3 m-auto mt-5 rounded">
       <form action="">
-        <div class="row">
-          <div class="col-3">
-            <select name="gender" id="inputState" class="form-select" required>
-              <option selected>Choose...</option>
+        <div className="row g-2">
+          <div className="col-md-3">
+            <select
+              name="gender"
+              id="inputState"
+              className="form-select"
+              required
+              defaultValue=""
+              onChange={handleOnChange}
+            >
+              <option value="">Choose...</option>
               <option value="m">Male</option>
               <option value="f">Female</option>
             </select>
           </div>
-          <div class="col-6">
+          <div className="col-md-6">
             <input
               name="name"
               type="text"
-              class="form-control"
+              className="form-control"
               placeholder="User name"
               aria-label="User name"
               required
+              onChange={handleOnChange}
             />
           </div>
-          <div className="col-3 d-grid">
+          <div className="col-md-3 d-grid">
             <button className="btn btn-primary">Add User</button>
           </div>
         </div>
